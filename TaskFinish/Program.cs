afc9a567779ac3eb9da133adc78fb8a4ce3f275e
Sergey <Sergey@yqndex.ru>
2023-03-23 15:16:53 +0300

    Программа

---
+++ TaskFinish/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.Clear();
+string[] Array1 = new string[7] {"Воронеж","Уфа","Москва","Обь","Лоо","Тверь","111"};
+string[] Array2 = new string[Array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(Array1, Array2);
+PrintArray(Array2);
